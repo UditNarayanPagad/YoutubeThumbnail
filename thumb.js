fbc55ac91035ed714b8e6387f4951648b510cf85
UditNarayanPagad <uditnarayanpagad@gmailcom>
2024-07-06 15:37:05 +0530

    text updated

--- thumb.js
+++ thumb.js
@@ -39,5 +39,3 @@ let createThumbnail = (nameI,chanelI,viewsI,timeI)=>{
 </div>`
    addBox.appendChild(newDiv);
 }
-
-// createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
