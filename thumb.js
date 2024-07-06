1055aeccffb180b0e9d4659b43db87d27e558fbe
UditNarayanPagad <uditnarayanpagad@gmailcom>
2024-07-06 15:48:15 +0530

    Url update system

--- thumb.js
+++ thumb.js
@@ -2,6 +2,7 @@ let name = document.querySelector("#name");
 let chanel = document.querySelector("#chanel");
 let views = document.querySelector("#views");
 let time = document.querySelector("#time");
+let url = document.querySelector("#url");
 let h = document.querySelector("h2");
 let addBox = document.querySelector(".addBox");
 
@@ -15,20 +16,21 @@ let takeInput = ()=>{
     let chanelI = document.getElementById('chanel').value;
     let viewsI = document.getElementById('views').value;
     let timeI = document.getElementById('time').value;
+    let urlI = document.getElementById('url').value;
     if(viewsI>=1000 && viewsI<1000000){
         viewsI = Math.floor((viewsI/1000) * 100) / 100 +"k";
     }
     else if(viewsI>=1000000){
         viewsI = Math.floor((viewsI/1000000) * 100) / 100 +"m";
     }
-    createThumbnail(nameI,chanelI,viewsI,timeI);
+    createThumbnail(nameI,chanelI,viewsI,timeI,urlI);
 }
 
-let createThumbnail = (nameI,chanelI,viewsI,timeI)=>{
+let createThumbnail = (nameI,chanelI,viewsI,timeI,urlI)=>{
    let newDiv = document.createElement('div');
    newDiv.innerHTML = `<div class="box">
    <div class="thumbnail">
-       <img src="thumbnail.webp" alt="">
+       <img src=${urlI} alt="">
    </div>
    <div class="desc">
       <h2>${nameI}</h2>
